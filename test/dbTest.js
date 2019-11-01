4b1426763ca134fe93b01d021a88d466b127664e
Olakunle Salami <kunsal2003@gmail.com>
2019-11-01 18:43:11 +0000

    Write test for database setup

---
+++ test/dbTest.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const db = require('../config/database');
+
+const { expect } = chai;
+
+describe('Database', () => {
+  it('should not return with connection error but with 2', (done) => {
+    db.query('SELECT 1 + 1 as value', (err, res) => {
+      const result = res.rows[0].value;
+      expect(result).to.equals(2);
+      done();
+    });
+  });
+});
