534919ea5187aa073180eb3bfe81623ae852f0c4
Olakunle Salami <kunsal2003@gmail.com>
2019-11-01 18:45:14 +0000

    Add airbnb linting for code quality

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
-})
+  // console.log(`App running on port ${port}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,8 @@
 const express = require('express');
 require('dotenv').config();
-const db = require('../config/database');
 
 const app = express();
-
 app.get('/', (req, res) => {
-  db.query('SELECT * from comments', (err, res) => {
-    console.log(res.rows)
-    db.end()
-  });
-  //console.log(res);
   res.send('Hello team');
 });
- 
 module.exports = app;
