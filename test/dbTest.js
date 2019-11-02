ffef1f64fed9012d2753627913958875c35c2b51
Olakunle Salami <kunsal2003@gmail.com>
2019-11-02 10:00:41 +0000

    Rewrite database integration test

--- test/dbTest.js
+++ test/dbTest.js
@@ -1,16 +1,14 @@
 const chai = require('chai');
-// const db = require('../config/database');
+const pool = require('../config/database');
 
 const { expect } = chai;
 
 describe('Database', () => {
-  it('should not return with connection error but with 2', (done) => {
-    // db.query('SELECT 1 + 1 as value', (err, res) => {
-    //   const result = res.rows[0].value;
-    //   expect(result).to.equals(2);
-    //   done();
-    // });
-    expect(1 + 1).to.equal(2);
-    done();
+  it('should not return with connection error', (done) => {
+    pool.connect((err, client, release) => {
+      expect(err).to.equal(undefined);
+      release();
+      done();
+    });
   });
 });
